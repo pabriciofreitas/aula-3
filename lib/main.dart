4a38f82f4d7281e99710d48bd9ce8e35e77c9b03
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-15 20:44:55 -0300

    feat: add botton navigation bar

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override //método responsavel por reconstruir a tela
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //const---ele não build isso navamente quando atualizar
+      //evitar cirar funções que retorne widgets use staff ou staless
+      //use constante em temas que n muda.
+      //locale: ,Formata data
+      //locale: Locale("pt", "BR"),
+      //supportedLocales: ,//lista de locale
+      //shortcuts: ,//atalhos
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Use acamdemy"),
+          centerTitle: true,
+          //titleSpacing: ,
+        ),
+        //
+        backgroundColor: const Color(0xFFFFFFFF),
+        //
+        //extendBodyBehindAppBar: , persistir botão mesmo se trocar de tela
+
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.grey[200],
+          selectedLabelStyle: const TextStyle(
+            color: Colors.white,
+          ),
+          unselectedLabelStyle: TextStyle(
+            color: Colors.blue[100],
+          ),
+          showSelectedLabels: true,
+          onTap: (value) => debugPrint(value.toString()),
+          items: [
+            BottomNavigationBarItem(
+              label: "Inicio",
+              icon: Icon(
+                Icons.home_outlined,
+                color: Colors.white,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: "Person",
+              icon: Icon(
+                Icons.person_outline_rounded,
+                color: Colors.white,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: "shop",
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
