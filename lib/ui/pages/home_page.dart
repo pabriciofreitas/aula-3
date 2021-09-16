851b6c1b4f1869205477075768319f5f63fc11f7
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-16 20:55:02 -0300

    feat: trabalhando com os bottões

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,22 +6,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerFloat, //localixação do button
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          //heroTag: ,
+          //isExtended: ,//expandirr botãspo
+          splashColor: Colors.red[400], //cor enquando segura
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+        ),
         body: SafeArea(
-      child: SizedBox.expand(
-        child: Container(
-          color: Colors.blue,
-          child: Transform.translate(
-            //.translate
-            //para rotacionar os componentes
-            //transform: ,
-            // transform: , //mudando pesctiva da imagem 3d
-            offset: Offset(22, 60),
+          child: SizedBox.expand(
             child: Container(
-              color: Colors.red,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        primary: Colors.red,
+                        onPrimary: Colors.white,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                      ),
+                      onLongPress: () => debugPrint("Pessionou e segurous"),
+                      onPressed: () => debugPrint("elevationButton onPress"),
+                      child: Text("elevationButton")),
+                  OutlinedButton(
+                      //trocar a bordar
+                      onPressed: () {},
+                      child: Text("outlinedbutton")),
+                  TextButton(onPressed: () {}, child: Text("textbutton"))
+                ],
+              ),
             ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
