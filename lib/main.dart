86e9cc941097f7e337c8d3d8013be0f835af77c8
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-15 22:08:15 -0300

    feat: added to scaffold with column and containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
             height: 30,
             color: Colors.red,
           ),
+          //title:align(alignment:aling.center)
           title: const Text("Use acamdemy"),
           centerTitle: true,
           //titleSpacing: ,
@@ -50,11 +52,29 @@ class MyApp extends StatelessWidget {
         ),
         //
         backgroundColor: const Color(0xFFFFFFFF),
-        //
+        //extedboby extende tamanho do body
+
+        body: SafeArea(
+          child: Column(
+            //mainAxisSize: , //tamanho máximo possivel
+            mainAxisAlignment: MainAxisAlignment
+                .center, //vertical space around.espaço dispomnive , evenlu
+            // crossAxisAlignment:
+            //   CrossAxisAlignment.stretch, //prencher hozizontal  //horizontal
+            //verticalDirection: VerticalDirection.up,//começa coluna de baixo
+            children: [
+              MyContainer(),
+              MyContainer(),
+              MyContainer(),
+            ],
+          ),
+        ),
+
         //extendBodyBehindAppBar: , persistir botão mesmo se trocar de tela
 
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Colors.grey[200],
+          selectedItemColor: Colors.blue,
           selectedLabelStyle: const TextStyle(
             color: Colors.white,
           ),
@@ -69,26 +89,81 @@ class MyApp extends StatelessWidget {
               label: "Inicio",
               icon: Icon(
                 Icons.home_outlined,
-                color: Colors.white,
+                //color: Colors.white,
               ),
             ),
             BottomNavigationBarItem(
               label: "Person",
               icon: Icon(
                 Icons.person_outline_rounded,
-                color: Colors.white,
+                //color: Colors.white,
               ),
             ),
             BottomNavigationBarItem(
               label: "shop",
               icon: Icon(
                 Icons.shopping_bag_outlined,
-                color: Colors.white,
+                //color: Colors.white,
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //  width: ,
+      height: 100,
+
+      margin: const EdgeInsets.all(24), //fora
+      padding: const EdgeInsets.all(16), //dentro
+      //alignment: Alignment.center,
+      decoration: BoxDecoration(
+        //desinger diz que bordar trás segurança par cliente
+        borderRadius: BorderRadius.circular(16),
+
+        boxShadow: [
+          BoxShadow(
+            color: Colors.blue.withOpacity(0.4),
+            offset: Offset(4.0, 4.0),
+            blurRadius: 6,
+          ),
+        ],
+        //shape: BoxShape,
+        color: Colors.green,
+      ),
+      child: Row(//mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+        const Icon(Icons.home),
+        Expanded(
+          child: Text(
+            "Meu containerffdsfdssfsdfdsfsd",
+            maxLines: 2,
+            overflow: TextOverflow
+                .ellipsis, //casp texto seja maior poem 3 pontos de continuo
+            //semanticsLabel: ,dolar
+            textAlign: TextAlign.center,
+            //textScaleFactor: 1.0, almentar fonte do texto // se não aumenta no systema operacional n mexe com esse coisa // no material tem isso
+            style: GoogleFonts.poppins(
+                fontSize: 50,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+                decoration: TextDecoration.lineThrough, //linha notextp
+                //decorationStyle: TextDecorationStyle.dashed
+                letterSpacing: 2.0),
+          ),
+        ),
+        const Icon(Icons.home),
+      ]),
     );
   }
 }
