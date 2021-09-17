b9e41fdf8c1df56508597d9704fc34b64e99ad27
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-17 20:26:57 -0300

    feat: StatefulWidget e StatelessWidget

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,16 +8,72 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
       child: SizedBox.expand(
+          child: Column(
+        children: [firstContainer(), SecondContainer()],
+      )),
+    ));
+  }
+}
+
+class SecondContainer extends StatefulWidget {
+  const SecondContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SecondContainer> createState() => _SecondContainerState();
+}
+
+class _SecondContainerState extends State<SecondContainer> {
+  late String name1;
+
+  @override
+  void initState() {
+    name1 = "Freitas";
+    //isso rodar no começo de tudo na renderização só pe chmado na inicialização da tela
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+          color: Colors.green,
+          child: Row(children: [
+            Text(name1),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    name1 = "Pedro";
+                  });
+                },
+                child: const Text("aaa"))
+          ])),
+    );
+  }
+}
+
+class firstContainer extends StatelessWidget {
+  firstContainer({
+    Key? key,
+  }) : super(key: key);
+  //só recebe construtor fixo final ou const
+  String name1 = "Pabricio";
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
         child: Container(
-          color: Colors.amber,
-          child: IgnorePointer(
-            //ignora o toque
-            child: ElevatedButton(
-                onPressed: () => debugPrint('apertou'), child: Text("aaaaaaa")),
-          ),
-          /* on pressed true? faz: retorna null*/
-        ),
-      ),
+      child: Center(
+          child: Row(children: [
+        Text(name1),
+        ElevatedButton(
+            onPressed: () {
+              name1 = "Pedro";
+            },
+            child: const Text("aaa"))
+      ])),
+      color: Colors.blue,
     ));
   }
 }
