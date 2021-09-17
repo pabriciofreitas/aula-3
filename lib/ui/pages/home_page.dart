675159ce3622f57cf4725ccd6e9ffbd0236e368c
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-17 19:51:51 -0300

    feat: IgnorePoint ignora detecção botão

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,33 +8,14 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
       child: SizedBox.expand(
-        child: ListView.separated(
-          itemBuilder: (context, index) => ListTile(
-            title: Text("Titulo"),
-            subtitle: Text("subTitulo"),
-            leading: CircleAvatar(
-              // backgroundColor: Colors.blue,
-              child: Text("T"),
-              backgroundImage:
-                  Image.asset("lib/assets/images/image01.jpg").image,
-              //onBackgroundImageError: , //caso a imagem der erro
-              //foregroundColor: , a foto por cima
-            ),
-            //trailing: InkWell(
-            //  onTap: () => debugPrint("on tap $index"),
-            //  child: Icon(Icons
-            //     .edit_outlined)), //depois do tituilo iconButton===MAIs completo
-            //enabled: ,se usaario consegue interagir com usuario
-            tileColor: Colors.blue[200],
-            //horizontalTitleGap: 32, espaçamento
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-            onTap: () => debugPrint("on tap $index"),
+        child: Container(
+          color: Colors.amber,
+          child: IgnorePointer(
+            //ignora o toque
+            child: ElevatedButton(
+                onPressed: () => debugPrint('apertou'), child: Text("aaaaaaa")),
           ),
-          separatorBuilder: (context, index) => const SizedBox(
-            height: 20,
-          ),
-          itemCount: 20,
+          /* on pressed true? faz: retorna null*/
         ),
       ),
     ));
