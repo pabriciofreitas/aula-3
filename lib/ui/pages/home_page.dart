8696425388a508142391e31975d53b95baca53dc
Pabricio Freitas <pabricio2000@gmail.com>
2021-09-16 21:23:51 -0300

    feat: usando stack

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,33 +6,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: SizedBox.expand(
-        child: Container(
-          color: Colors.white,
-          child: IndexedStack(
-            alignment: Alignment.center,
-            index: 0,
-            children: [
-              Container(
-                height: 400,
-                width: 300,
-                color: Colors.red,
-              ),
-              Container(
-                height: 400,
-                width: 300,
-                color: Colors.green,
-              ),
-              Container(
-                height: 400,
-                width: 300,
-                color: Colors.amber,
-              )
-            ],
-          ),
+      body: SafeArea(
+        child: SizedBox.expand(
+          child: Container(
+              color: Colors.white,
+              child: Stack(alignment: Alignment.center,
+                  // fit: StackFit.expand, //fit redimenciona os filhos
+
+                  children: [
+                    Positioned(
+                        left: 0,
+                        top: 0,
+                        height: 100,
+                        width: 200,
+                        child: Container(
+                          color: Colors.pink,
+                        )),
+                    Container(
+                      constraints: BoxConstraints(
+                        minHeight: 400,
+                        minWidth: 300,
+                      ),
+                      height: 400,
+                      width: 300,
+                      color: Colors.amber,
+                    ),
+                  ])),
         ),
       ),
-    ));
+    );
   }
 }
